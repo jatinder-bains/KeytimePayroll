ac15bba8ddfa8c106b8593546058e04b87d6c902
jsbains <jsbains74@gmail.com>
2017-01-04 21:54:40 +0000

    Rename data access assembly, namespace and classes

--- KeytimePayrollDataAccess/IKeytimePayrollDataAccess.cs
+++ KeytimePayrollDataAccess/IKeytimePayrollDataAccess.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KeytimePayrollDataAccess
+namespace KeytimePayroll.DataAccess
 {
-    public class Class1
+    interface IKeytimePayrollDataAccess
     {
+
     }
 }
